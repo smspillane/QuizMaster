1e98b9efd56011a21135a22c9570519cb771be2c
Samuel Spillane <samuel.m.spillane@gmail.com>
2014-03-22 18:40:52 +1100

    Not as Breakable

--- quizmaster.py
+++ quizmaster.py
@@ -44,17 +44,23 @@ def newround(x,name):
 def roundone(name):
 	subtract = 0
 	total = 0
+
 	print 'Alright ' + name + '.  Time to get started.  This round is going to test your ability to estimate.  I am going to provide you with a number of descriptions.  Such as, the number of eggs you have if you buy a dozen.  Of course, this value is 12.  If you were to guess correctly, for instance, 12, I would take off proportionate number of points.  However, if you were to guess less than this number you would lose less points.  If you were to overestimate, however, you would end up losing no points.  You want to guess close, without going over.\n\n'
 	print 'NOTE: Each question is scaled so that if you were to guess a number correctly, you would lose 20 points.  There will be 10 questions, and thus you are able to lose up to 200 points this round.  Good luck ' + name + '!\n\n'
 	for row in R1reader:
 		print row[2]
 		guess = raw_input("So, what is your guess?  Remember, digits only!\n")
-		if int(guess) > int(row[0]):
-			print "Woops!  You overestimated!  No losing points for you!  The answer was " + row[0] + "!"
-		else:
-			subtract = float(guess) * (20/(float(row[0])))
-			print 'Congratulations.  The answer was ' + row[0] + ' and for your effort you get to lose ' + str(subtract) + ' points!'
-			total = total + subtract
+		x = 1
+		try:
+			x = int(guess)
+			if int(guess) > int(row[0]):
+				print "Woops!  You overestimated!  No losing points for you!  The answer was " + row[0] + "!"
+			else:
+				subtract = float(guess) * (20/(float(row[0])))
+				print 'Congratulations.  The answer was ' + row[0] + ' and for your effort you get to lose ' + str(subtract) + ' points!'
+				total = total + subtract
+		except:
+			print str(guess) + ' is not a number!  You lose 0 points!'
 	print 'The round is over!  You managed to lose ' + str(total) + ' points!  Given that you could have lost up to 200 points, you have lost ' + str((total/200)*100) + '% of what you could have lost!'
 	newround(2, name)
 
@@ -67,11 +73,15 @@ def roundtwo(name):
 		total += 1
 		print 'Which of the following is ' + row[2] + '?\n  1. ' + row[0] + ' or 2. ' + row[1] + '?'
 		guess = raw_input("So, what is your guess?  Put in the corresponding digit - 1 or 2, only.\n")
-		if guess == row[3]:
-			print 'Correct!  Congratulations!'
-			correct += 1
-		else:
-			print 'Sorry, that is incorrect.'
+		try:
+			int(guess) in [1,2]
+			if guess == row[3]:
+				print 'Correct!  Congratulations!'
+				correct += 1
+			else:
+				print 'Sorry, that is incorrect.'
+		except:
+			print "That is not a valid option!  You are getting this one wrong as punishment!"
 	num = ((float(correct))/float(total)) * 200
 	print 'I asked you ' + str(total) + ' questions and you lose ' + str(correct) + ' of those correct!  Thus, this round you score ' + str(num) + ' points!  That is ' + str(num/2) + '% of what you could have lost!'
 	newround(3,name)
